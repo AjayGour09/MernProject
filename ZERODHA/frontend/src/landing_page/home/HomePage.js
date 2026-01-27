31de8199d0ee0aac4a51388f774e1c1d37cc881e
Ajay Gour <ajaygour369@gmail.com>
2026-01-27 10:49:10 +0530

    Add new components for landing page sections
    - Introduced Brokerage, Hero, and PricingPage components in the pricing directory.
    - Added Hero, LeftSection, ProductPage, RightSection, and Universe components in the products directory.
    - Created SignUp component in the signup directory.
    - Implemented CreateTicket and Hero components in the support directory.
    - Added SupportPage component in the support directory.
    - Included a new image file for zerodhaFundhouse.

---
+++ ZERODHA/frontend/src/landing_page/home/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import NavBar from '../components/NavBar.js'
+import Hero from './Hero.js'
+import Award from './Award.js'
+import Stats from './Stats.js'
+import Pricing from "./Pricing.js";
+import Education from './Education.js';
+import OpenAccount from '../components/OpenAccount.js'
+import Footer from '../components/Footer.js'
+const HomePage = () => {
+  return (
+    <>
+    
+    <NavBar />
+    <Hero />
+    <Award />
+    <Stats />
+    <Pricing />
+    <Education />
+    <OpenAccount />
+    <Footer />
+    </>
+  )
+}
+
+export default HomePage
