5d1e0eaa71d09690e1105d3a378f5c1f6a158c41
Ajay Gour <ajaygour369@gmail.com>
2026-06-07 21:35:56 +0530

    Add Auth and Home page components
    - Created Auth.jsx for the authentication page with a logo and title.
    - Created Home.jsx as a placeholder for the home page.

--- AIInterview/client/src/App.jsx
+++ AIInterview/client/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Route, Router } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Auth from './pages/Auth'
 
 function App () {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/auth' element={<Auth/>}/>
+      </Router>
+    </div>
   )
 }
 export default App
---
+++ AIInterview/client/src/pages/Auth.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BsRobot} from "react-icons/bs"
+
+function Auth() {
+  return (
+    <div className='w-full min-h-screen bg-[#f3f3f3] flex items-center justify-center px-16 py-20'>
+        <div className='w-full max-w-md p-8 rounded-3xl bg-white shadow-2xl border border-grey-200'>
+            <div className='flex items-center justify-center gap-3 mb-6'>
+                <div className='bg-black text-white p-2 rounded-lg'>
+                    <BsRobot size={18}/>
+                </div>
+                <h2 className='font-semibold text-lg'>InterviewIQ.AI</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Auth
