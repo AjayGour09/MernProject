74753c4eab8bf9f958318df8afa02837f4b05a57
Ajay Gour <ajaygour369@gmail.com>
2026-06-18 22:03:42 +0530

    implement user authentication and current user retrieval

--- AIInterview/client/src/App.jsx
+++ AIInterview/client/src/App.jsx
@@ -2,10 +2,26 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
+import { useEffect } from "react";
+import axios from "axios";
 
 export const serverURL = "http://localhost:8080"
 
 function App() {
+
+  useEffect(()=>{
+    const getUser = async()=>{
+      try {
+        const result = await axios.get(serverURL + "/api/user/current-user", {withCredentials:true})
+        console.log(result.data);
+        
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
+    getUser()
+  },[])
   return (
     <div>
       <Routes>
